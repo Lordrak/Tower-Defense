763d57e9542155715bf613a3e73b4c35a01d304f
Lordrak <rudyraffin@gmail.com>
2014-12-07 21:31:41 +0100

    detection ver 1
    detecte la sphere qui tombe puis la supprime

---
+++ Detection/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class NewBehaviourScript : MonoBehaviour {
+	[SerializeField]
+	Transform _cube;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Adversaire") {
+			other.gameObject.GetComponent<Adversaire> ().setVie (1); 
+			if (other.gameObject.GetComponent<Adversaire> ().getVie () <= 0) {
+				Destroy (other.gameObject);
+			}
+
+		} 
+	}
+}
