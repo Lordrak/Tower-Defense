7e10b5299092bcdacf520e8319650f842d368b64
ntarradeESGI <nico.tarrade@free.fr>
2014-12-16 10:51:47 +0100

    Pasfiding
    Ajout du déplacement des monstres

---
+++ Code/map/Assets/scripts/deplacement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class deplacement : MonoBehaviour {
+
+	[SerializeField]
+	NavMeshAgent _agent;
+
+	[SerializeField]
+	Transform _destination;
+
+	// Use this for initialization
+	void Start () {
+		_agent.SetDestination(_destination.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
