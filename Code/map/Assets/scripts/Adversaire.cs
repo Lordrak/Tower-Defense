30edc56a3fb02e9fabea7f9950476a395f81b8fd
Lordrak <rudyraffin@gmail.com>
2014-12-16 22:49:35 +0100

    ver2 pas finding

---
+++ Code/map/Assets/scripts/Adversaire.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Adversaire : MonoBehaviour {
+	[SerializeField]
+	public int _vie;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void setVie(int degat){
+		_vie = _vie - degat;
+	}
+	public int getVie(){
+		return _vie;
+	}
+}
