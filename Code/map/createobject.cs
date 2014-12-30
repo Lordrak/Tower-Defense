cac48d5c2299fd6016527a28e45a9dcb748ffe94
ntarradeESGI <nico.tarrade@free.fr>
2014-12-30 19:13:31 +0100

    code placement
    code du placement des tourelles

---
+++ Code/map/createobject.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class createobject : MonoBehaviour {
+
+	[SerializeField]
+	Transform testobject;
+
+	Ray ray;
+		RaycastHit hit;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey("a")) {
+			ray=Camera.main.ScreenPointToRay(Input.mousePosition);
+			
+			if(Physics.Raycast(ray,out hit))
+			{
+				
+				if(Input.GetKey(KeyCode.Mouse0))
+				{
+					Instantiate (testobject, new Vector3(hit.point.x, hit.point.y, hit.point.z), Quaternion.identity);
+				}
+		}
+	
+}
+/*public void createcube()
+	{
+			
+		}*/
+}
+}
