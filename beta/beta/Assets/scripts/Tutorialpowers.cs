656f5ac6fd7e79c8b331c51d6bc30ac1eed33f86
ntarradeESGI <nico.tarrade@free.fr>
2015-04-26 11:10:50 +0200

    shaders et tuto pouvoir

---
+++ beta/beta/Assets/scripts/Tutorialpowers.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tutorialpowers : MonoBehaviour {
+
+	string instruction;
+	string instructions;
+	string description;
+	string pouvoirpassif;
+	string pouvoiractif;
+	string pouvoirdeboost;
+	string pouvoirmulti;
+	string mana;
+
+	// Use this for initialization
+	void Start () {
+		instructions = "Bienvenue dans le tutoriel sur les pouvoirs";
+		instruction = instructions;
+		mana = "Tuer des monstres rapporte du mana qui permet après en voir gagné un certain nombrede lancer des pouvoirs divins";
+		description = "Quelque soit le dieu que vous utilisez, vous avez 4 pouvoir à votre disposition";
+		pouvoirpassif = "Le pouvoir passif booste vous apporte un bonus de stats permanent pour toute la partie, celui de Geb ici augmente la vie maximale de l'idole";
+		pouvoiractif = "Le pouvoir actif est un sort touchant tous les ennemies présent sur la carte et leurs infligeant d'importants dégats";
+		pouvoirdeboost = "Le pouvoir de boost renforce les unités que vous avez posés sur le terrain jusqu'a la fin de la vague et leurs ajoute un effet sur l'attaque";
+		pouvoirmulti = "Uniquement en partie multijoueur, ce pouvoir permet d'envoyer un Boss à l'autre joueur";
+	}
+
+	void OnGUI()
+	{
+		GUI.Label (new Rect (500, 150, 100, 25), instruction);
+		if (instruction == instructions) {
+			if (GUI.Button (new Rect (500, 150, 100, 25), "Passer")) {
+				instruction = mana;
+			}
+		}
+		if (instruction == mana) {
+			if (GUI.Button (new Rect (500, 150, 100, 25), "Passer")) {
+				instruction = description;
+			}
+		}
+		if (instruction == description) {
+			if (GUI.Button (new Rect (500, 150, 100, 25), "Passer")) {
+				instruction = pouvoirpassif;
+			}
+		}
+		if (instruction == pouvoirpassif) {
+			if (GUI.Button (new Rect (500, 150, 100, 25), "Passer")) {
+				instruction = pouvoiractif;
+				GUI.Button(new Rect())
+			}
+		}
+		if (instruction == pouvoiractif) {
+			if (GUI.Button (new Rect (500, 150, 100, 25), "Passer")) {
+				instruction = pouvoirdeboost;
+			}
+		}
+		if (instruction == pouvoirdeboost) {
+			if (GUI.Button (new Rect (500, 150, 100, 25), "Passer")) {
+				instruction = pouvoirmulti;
+			}
+		}
+	}
+
+}
---
+++ beta/beta/Assets/scripts/lifebarre.shader
@@ -0,0 +1,60 @@
+﻿Shader "Custom/lifebarre" {
+   Properties //
+   {
+      _Color ("Main Color", Color) = (1,1,1,0.5)
+      _CutValue ("Bar value", Range (0, 0.99)) = 0.99
+      _MainTex ("Main Texture", 2D) = ""
+      _Gradient ("Gradient", 2D) = ""
+      _Border ("Border", 2D) = ""
+    }
+   SubShader
+   {
+         Blend SrcAlpha OneMinusSrcAlpha
+         Tags {Queue=Transparent}
+       
+      Pass
+      {
+         CGPROGRAM
+         #pragma vertex vert
+         #pragma fragment frag
+         #include "UnityCG.cginc"
+         
+         float4       _Color;
+         float       _CutValue;
+         sampler2D    _MainTex;
+         sampler2D    _Gradient;
+         sampler2D    _Border;
+         
+         uniform float4 _MainTex_ST;
+         uniform float4 _Gradient_ST;
+         uniform float4 _Border_ST;
+         
+         struct v2f {
+             float4 pos : SV_POSITION;
+            float2  UV_MainTex : TEXCOORD0;
+            float2  UV_Gradient : TEXCOORD1;
+            float2  UV_Border : TEXCOORD2;
+         };
+         
+         v2f vert (appdata_base v)
+         {
+             v2f o;
+             o.pos = mul (UNITY_MATRIX_MVP, v.vertex);
+             o.UV_MainTex = TRANSFORM_TEX (v.texcoord, _MainTex);
+            o.UV_Gradient = TRANSFORM_TEX (v.texcoord, _Gradient);
+            o.UV_Border = TRANSFORM_TEX (v.texcoord, _Border);
+             return o;
+         }
+   
+         half4 frag (v2f i) : COLOR
+         {
+            half4 borderColor = tex2D(_Border, i.UV_Border);
+            half4 barColor = float4(0, 0, 0, 0);
+            if (tex2D(_Gradient, i.UV_Gradient).x < _CutValue)
+                 barColor = tex2D(_MainTex, i.UV_MainTex) * _Color;
+             return barColor   (1 - borderColor.a) + borderColor  borderColor.a;
+         }
+         ENDCG
+      }
+   }
+}
